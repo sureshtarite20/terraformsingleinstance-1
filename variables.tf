aac78bb7a0482d237e6076923db2218267e327e1
sureshtarite20 <79962213+sureshtarite20@users.noreply.github.com>
2021-03-19 23:10:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+#variable "aws_access_key" {}
+#variable "aws_secret_key" {}
 variable "aws_region" {}
 variable "amis" {
     description = "AMIs by region"
@@ -33,8 +33,8 @@ variable "instance_type" {
   type = "map"
   default = {
     dev = "t2.nano"
-    test = "t2.micro"
-    prod = "t2.medium"
+    #test = "t2.micro"
+    #prod = "t2.medium"
     }
 }
 
