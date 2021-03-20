67e83374dcdff321bbb7482dbab7c69cbf016f44
sureshtarite20 <79962213+sureshtarite20@users.noreply.github.com>
2021-03-20 15:48:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -107,23 +107,23 @@ terraform {
 # }
 
 
-# resource "aws_instance" "web-1" {
-#     ami = var.imagename
-#     #ami = "ami-0d857ff0f5fc4e03b"
-#     #ami = "${data.aws_ami.my_ami.id}"
-#     availability_zone = "us-east-1a"
-#     instance_type = "t2.micro"
-#     key_name = "LaptopKey"
-#     subnet_id = "${aws_subnet.subnet1-public.id}"
-#     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
-#     associate_public_ip_address = true	
-#     tags = {
-#         Name = "Server-1"
-#         Env = "Prod"
-#         Owner = "Sree"
-# 	CostCenter = "ABCD"
-#     }
-# }
+resource "aws_instance" "web-1" {
+    ami = var.imagename
+    #ami = "ami-0d857ff0f5fc4e03b"
+    #ami = "${data.aws_ami.my_ami.id}"
+    availability_zone = "us-east-1a"
+    instance_type = "t2.micro"
+    key_name = "devops-hp-laptop-key"
+    subnet_id = "${aws_subnet.subnet1-public.id}"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    tags = {
+        Name = "Server-1"
+        Env = "Dev"
+        Owner = "Suresh"
+	CostCenter = "ABCD"
+    }
+}
 
 ##output "ami_id" {
 #  value = "${data.aws_ami.my_ami.id}"
