10bfde9b68523bbcdb573416ca691c84f65839c9
sureshtarite20 <79962213+sureshtarite20@users.noreply.github.com>
2021-03-20 19:38:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,14 +3,7 @@
 variable "aws_region" {}
 variable "amis" {
     description = "AMIs by region"
-    default = {
-          us-east-1 = "ami-013f17f36f8b1fefb" #ubuntu 18.04 LTS
-	    #"ami-042e8287309f5df03" #ubuntu 20 LTS
-   		 # us-east-1 = "ami-97785bed" # ubuntu 14.04 LTS
-		# us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
-		# us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
-		# us-west-2 = "ami-f2d3638a" # ubuntu 14.04 LTS
-}
+    default = { us-east-1 = "ami-013f17f36f8b1fefb" }
 variable "vpc_cidr" {}
 variable "vpc_name" {}
 variable "IGW_name" {}
