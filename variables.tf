cd9ff35cc0e804cfb5e2e9cde63d67fccd1a605c
sureshtarite20 <79962213+sureshtarite20@users.noreply.github.com>
2021-03-21 02:07:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 #variable "aws_access_key" {}
 #variable "aws_secret_key" {}
 variable "aws_region" {}
-variable "amis" {
-    description = "AMIs by region"
-    default = { us-east-1 = "ami-013f17f36f8b1fefb" }
+# variable "amis" {
+#     description = "AMIs by region"
+#     default = { us-east-1 = "ami-013f17f36f8b1fefb" }
 variable "vpc_cidr" {}
 variable "vpc_name" {}
 variable "IGW_name" {}
@@ -17,18 +17,18 @@ variable "public_subnet2_name" {}
 variable "public_subnet3_name" {}
 variable "private_subnet_name" {}
 variable Main_Routing_Table {}
-variable "azs" {
-  description = "Run the EC2 Instances in these Availability Zones"
-  type = "list"
-  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
-}
+# variable "azs" {
+#   description = "Run the EC2 Instances in these Availability Zones"
+#   type = "list"
+#   default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+# }
 variable "environment" { default = "dev" }
-variable "instance_type" {
-  type = "map"
-  default = {
-    dev = "t2.nano"
-    #test = "t2.micro"
-    #prod = "t2.medium"
-    }
-}
+# variable "instance_type" {
+#   type = "map"
+#   default = {
+#     dev = "t2.nano"
+#     #test = "t2.micro"
+#     #prod = "t2.medium"
+#     }
+# }
 
